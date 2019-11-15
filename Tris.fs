17a8304842759fdcb8342ccf2299b90dafe9f441
Matteo Pagano <metiupaga8@gmail.com>
2019-11-15 23:38:47 +0100

    Update Tris.fs

--- Tris.fs
+++ Tris.fs
@@ -1,4 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
+// Learn more about F# at http://fsharp.org
 
 // Gioco scritto da Matteo Pagano
 
@@ -16,22 +16,22 @@ let rec insert g p l=
                  else
                  'o'::List.tail(l)
          |(_,x::xs)->x::(insert g (p-1) xs)
-         
-let refString l = sprintf (" %c | %c | %c \n------------\n %c | %c | %c \n------------\n %c | %c | %c " )
-                      (take_element 0 l)
-                      (take_element 1 l)
-                      (take_element 2 l)
-                      (take_element 3 l)
-                      (take_element 4 l)
-                      (take_element 5 l)
-                      (take_element 6 l)
-                      (take_element 7 l)
-                      (take_element 8 l)
-                      
-let tris = [' ';' ';' ';' ';' ';' ';' ';' ';' ']
-let spiegazioneTris = refString ['0';'1';'2';'3';'4';'5';'6';'7';'8']
-let trisInitial = refString tris
 
+let listTris l = 
+    sprintf (" %c | %c | %c \n------------\n %c | %c | %c \n------------\n %c | %c | %c " )
+        (take_element 0 l)
+        (take_element 1 l)
+        (take_element 2 l)
+        (take_element 3 l)
+        (take_element 4 l)
+        (take_element 5 l)
+        (take_element 6 l)
+        (take_element 7 l)
+        (take_element 8 l)
+
+let spiegazioneTris = listTris ['0';'1';'2';'3';'4';'5';'6';'7';'8']
+let tris = [' ';' ';' ';' ';' ';' ';' ';' ';' ']
+let trisInitial = listTris tris
 
 
 
@@ -60,44 +60,36 @@ let verificaVittoriaY l=
                 |_ ->false
 
 
-let rec giocoTris x l = 
-    match x with 
-        4-> printfn "Giocatore X:"
-            let giocatoreX='x'
-            let a = int32(System.Console.ReadLine()) 
-            let refreshTris= (insert (giocatoreX) (a) (l))
-            System.Console.Clear()
-            printfn "%s" (refString refreshTris)
-            if(verificaVittoriaX refreshTris) 
-            then giocoTris 10 refreshTris
-            else printfn "\nPareggio !"
-        |10-> printfn "\nHa vinto il giocatore X"
-        |11-> printfn "\nHa vinto il giocatore Y"
-        |_-> let giocatoreX='x'
-             let giocatoreY='o'
-             printfn "Giocatore X:"
-             let a = int32(System.Console.ReadLine()) 
-             let refreshTris= (insert (giocatoreX) (a) (l))
-             System.Console.Clear()
-             printfn "%s" (refString refreshTris)
-             if(verificaVittoriaX refreshTris) 
-                then giocoTris 10 refreshTris
-             else
-                printfn "Giocatore Y:"
-                let b = int32(System.Console.ReadLine()) 
-                let refreshTris= (insert (giocatoreY) (b) (refreshTris))
-                System.Console.Clear()
-                printfn "%s" (refString refreshTris)
-                if(verificaVittoriaY refreshTris) 
-                then 
-                    giocoTris 11 refreshTris
-                else
-                    giocoTris (x+1) refreshTris
-          
-          
+
+
+let rec giocoTris turno tris giocatore= 
+    match turno with 
+        9-> printfn "\nPareggio !"
+        |10 -> if (giocatore='x') then printfn "\nHa vinto il giocatore X"
+                  else printfn "\nHa vinto il giocatore Y"
+        |_-> match giocatore with
+                   'x' -> let giocatoreX='x'
+                          printfn "Giocatore X:"
+                          let a = int32(System.Console.ReadLine()) 
+                          let refreshTris= (insert (giocatoreX) (a) (tris))
+                          System.Console.Clear()
+                          printfn "%s" (listTris refreshTris)
+                          if(verificaVittoriaX refreshTris) 
+                              then giocoTris 10 refreshTris 'x'
+                          else
+                              giocoTris (turno+1) refreshTris 'y'
+                   |'y' -> let giocatoreY='y'
+                           printfn "Giocatore Y:"
+                           let b = int32(System.Console.ReadLine()) 
+                           let refreshTris= (insert (giocatoreY) (b) (tris))
+                           System.Console.Clear()
+                           printfn "%s" (listTris refreshTris)
+                           if(verificaVittoriaY refreshTris) 
+                               then giocoTris 10 refreshTris 'y'
+                           else
+                           giocoTris (turno+1) refreshTris 'x'
+        
 [<EntryPoint>]
-
-
 
 printfn "COME GIOCARE:"
 printfn "Ad ogni turno inserire la posizione (intero) dove si vuole mettere la X o O"
@@ -107,4 +99,4 @@ printfn "Premi un qualsiasi tasto per iniziare..."
 Console.ReadLine();
 System.Console.Clear()
 printfn "%s" trisInitial
-giocoTris 0 (tris)
+giocoTris 0 (tris) 'x'

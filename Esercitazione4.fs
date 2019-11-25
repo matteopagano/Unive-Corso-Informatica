848cba80387c17d98ed3f60928ecd3222eb2594f
Matteo Pagano <metiupaga8@gmail.com>
2019-11-25 17:37:59 +0100

    Add files via upload

---
+++ Esercitazione4.fs
@@ -0,0 +1,34 @@
+﻿module Impl
+
+let NomeCognome : string = "Matteo Pagano" // <---- SCRIVI IN QUESTA STRINGA IL TUO NOME E COGNOME
+
+(*
+Data una funzione f ed una lista l, restituisce una nuova lista i cui elementi sono il risultato  di f per ogni elemento di l.
+*)
+let rec map (f:'a -> 'b)  (l:'a list): 'b list = 
+    match l with    
+        []->[]
+        |x::xs->(f x)::(map f xs)
+   
+(*
+Data una funzione f, un accumulatore acc ed una lista l effettua l'operazione di reduce. Ovvero, per ogni elemento x in l 
+applica f su acc e x e assegna il valore ottenuto all'accumulatore. Per esempio, se la funzione in input è f, gli
+elementi sono i0, i1, i2 e lo stato iniziale è s allora il risultato della computazione sarà 
+f (f (f s i0) i1) i2.
+*)
+let rec fold (f:'State -> 'T -> 'State) (acc: 'State) (l:'a list): 'State = 
+    match l with
+        []->failwith "Lista vuota!"
+        |x::xs->fold f (f acc x) xs
+
+(*
+Data una funzione f ed una lista l applica f ad ogni elemento di l.
+*)
+let rec iter (f:'a -> unit)  (l:'a list): unit = 
+    match l with    
+        []-> ()
+        |x::xs->f x
+                iter f xs
+
+
+
